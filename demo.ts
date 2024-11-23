b3f47f683885cbf7b97c653365a67f7dd401a474
Adene Daniel <philocity@proton.me>
2024-11-23 20:03:46 +0000

    Update demo.ts

---
+++ demo.ts
@@ -0,0 +1,8 @@
+const bird = {
+    name: "turkey",
+    sound: "gobble gobble",
+    pet: false,
+    flight: false,
+    food: true}
+function birdQuiz() {
+    let birdAns = prompt("What sound do turkeys make")}
